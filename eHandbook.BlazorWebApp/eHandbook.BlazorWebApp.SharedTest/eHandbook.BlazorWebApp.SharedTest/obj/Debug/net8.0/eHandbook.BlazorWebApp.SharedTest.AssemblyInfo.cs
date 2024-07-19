51ac741e99d5a3233b49575a780bdfde826e3e51
Dariem Carlos Macias Mora <31046315+dariemcarlosdev@users.noreply.github.com>
2024-07-19 11:07:09 -0400

    Delete, no working due Secret configuration.Github Action WorkFlow created for CI/CD on Azure using GitHub Actions.

--- eHandbook.BlazorWebApp/eHandbook.BlazorWebApp.SharedTest/eHandbook.BlazorWebApp.SharedTest/obj/Debug/net8.0/eHandbook.BlazorWebApp.SharedTest.AssemblyInfo.cs
+++ eHandbook.BlazorWebApp/eHandbook.BlazorWebApp.SharedTest/eHandbook.BlazorWebApp.SharedTest/obj/Debug/net8.0/eHandbook.BlazorWebApp.SharedTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eHandbook.BlazorWebApp.SharedTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c46956f8f41550ba5516f52343a069f4df8f97bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("eHandbook.BlazorWebApp.SharedTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eHandbook.BlazorWebApp.SharedTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
