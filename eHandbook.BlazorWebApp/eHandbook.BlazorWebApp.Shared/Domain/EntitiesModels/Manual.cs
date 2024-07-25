bbaa7efec0b1b62cc89ffeccfcfb7e9b7d7b7052
Dariem Carlos Macias Mora <31046315+dariemcarlosdev@users.noreply.github.com>
2024-07-25 15:54:57 -0400

    ### Summary of Changes
    The primary change involves modifying the `Manual` record within the `eHandbook.BlazorWebApp.Shared.Domain.Entities` namespace. The `AuditableDetails` property, which was previously non-nullable, has been updated to be nullable. This adjustment allows the `AuditableDetails` property to be `null`, thereby increasing the flexibility of the `Manual` record by accommodating instances where `AuditableDetails` might not be provided.
    
    ### List of Changes
    
    1. **Change to `Manual` Record:**
       - **Property Affected:** `AuditableDetails`
       - **Modification:** Changed from a non-nullable type to a nullable type.
       - **Impact:** `AuditableDetails` can now be `null`, allowing for greater flexibility in instances where this detail is not available.
       - **Reference:** `eHandbook.BlazorWebApp.Shared.Domain.Entities.Manual`

--- eHandbook.BlazorWebApp/eHandbook.BlazorWebApp.Shared/Domain/EntitiesModels/Manual.cs
+++ eHandbook.BlazorWebApp/eHandbook.BlazorWebApp.Shared/Domain/EntitiesModels/Manual.cs
@@ -2,6 +2,6 @@
 {
 
     //Records are immutable by default, which means that once you create a record, you can't change its properties.
-    public record Manual(Guid Id, string? Description, string? Path, AuditableDetails AuditableDetails);
+    public record Manual(Guid Id, string? Description, string? Path, AuditableDetails? AuditableDetails);
 
 }
